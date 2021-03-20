a9e08a3568080c326c2790997e0025f67621d7e2
Alex Wiench <alex@alexwiench.com>
2021-03-20 16:39:17 -0500

    Simplified finding or creating .ssh folder.

--- updatekeys.sh
+++ updatekeys.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+
+#what does this do?
 IFS=$'\n'
 
 _dependencyCheck () { 
-  dependencies=( "read" "test" "type" "grep" "curl" "logname" "id")
-for i in "${dependencies[@]}"; do
+  dependencies=( "read" "test" "type" "grep" "curl" "logname" "id" "touch")
+  for i in "${dependencies[@]}"; do
     if ! type "$i" >/dev/null 2>&1; then
-    echo "Missing a dependency! Please install $i before using this script."
-exit 1
-fi
+      echo "Missing a dependency! Please install $i before using this script."
+      exit 1
+    fi
   done
 }
 
@@ -15,71 +17,58 @@ _isUserRoot() {
   if [ "$(id -un)" != "$(logname)" ]; then
       echo "This script is not designed to be run with sudo."
       exit 1
-fi
+  fi
 }
 
+_sshLocation(){
+  unset userinput
+  if [ ! -f "$HOME/.ssh/authorized_keys" ]; then
+      echo "$HOME/.ssh/authorized_keys does not exist. Create it? (Y/N)"
+      read -n 1 -r userinput
+      if [[ ! $userinput =~ ^[Yy]$ ]]; then
+          echo "Can not add keys without creating $HOME/.ssh/authorized_keys. Script will exit now."
+          exit 1
+      else
+          if [ ! -d "$HOME/.ssh/" ]; then
+              mkdir "$HOME/.ssh/"
+          fi
+          touch "$HOME/.ssh/authorized_keys"
+      fi
+  fi
+}
+
+_githubUsername () {
+  if [ "$1" != "" ]; then
+    githubname="$1"
+  else
+    echo "What is your Github username?"
+    read -r githubname
+  fi
+  #sanitize input
+  githubname=${githubname//[^a-zA-Z0-9-]/}
+}
+
+#THIS IS THE ISSUE. SSH-RSA ISN'T SOMETHING I CAN ASSUME ANYMORE.
 #Gets Keys
-pubkeys=$(curl -s https://github.com/$githubname.keys | grep "ssh-rsa")
-#grep just protects from curl garbage if it returns an unuseful response
+_downloadKeys () {
+  pubkeys=$(curl -s https://github.com/$githubname.keys | grep "ssh-rsa")
+  #grep just protects from curl garbage if it returns an unuseful response
 
-#Check username validity
-if [ "$pubkeys" = "Not Found" ]; then
-  echo "No keys found for $githubname."
-  echo "Check your spelling or add public keys to Github here https://github.com/settings/keys"
-  exit 1
-fi
-
-echo "Importing public keys from $githubname"
-
-#find path to .ssh folder
-numberofssh=$( find /home -name ".ssh" 2>/dev/null | grep -c '.ssh' ) 
-
-#find out if there are multiple .ssh folders
-if [ "$numberofssh" == 0 ]; then
-  #create directory
-  pathtossh="/home/$(logname)/.ssh"
-  echo "Can't find .ssh folder, will create one at "$pathtossh""
-  mkdir $pathtossh
-
-  elif [ "$numberofssh" == 1 ]; then
-  #use directory
-  pathtossh=$(find /home -name ".ssh" 2>/dev/null)
-
-  else
-  #pick directory
-  echo "Which location do you want to add keys too? (type a number)"
-    for sshlocation in $(find /home -name ".ssh" 2>/dev/null); do
-    j=$(( $j + 1))
-    echo "$j. "$sshlocation""
-    done
-
-  read sshlocationchoice
-
-  #validate input or at least let it crash more gracefully
-  if [ "$sshlocationchoice" -le "$j" ]; then
-    echo ""
-  else
-    echo "Try typing a displayed number."
+  #Check username validity
+  if [ "$pubkeys" = "Not Found" ]; then
+    echo "No keys found for $githubname."
+    echo "Check your spelling or add public keys to Github here https://github.com/settings/keys"
     exit 1
   fi
-    #count up to picked directory
-    for sshlocation in $(find /home -name ".ssh" 2>/dev/null); do
-    k=$(( $k + 1))
-      if [ $k == $sshlocationchoice ]; then
-      pathtossh="$sshlocation"
-      fi
-    done
 
-fi
+  echo "Importing public keys from $githubname"
+}
 
+
+_writeKeys() {
 authorized_keys="$pathtossh"/authorized_keys
 keyalreadyexisted=0
 keyadded=0
-
-#Sees if authorized_key file exists
-if ! test -e "$authorized_keys"; then
-  touch "$authorized_keys"
-fi
 
 for key in $pubkeys; do
   if grep -qxF "$key" "$authorized_keys"; then
@@ -95,4 +84,16 @@ done
 
 #Status
 echo "$keyadded keys were added to "$authorized_keys". $keyalreadyexisted keys already existed."
-exit 0
+}
+ 
+ _runscript(){
+   _dependencyCheck
+   _isUserRoot
+   _sshLocation
+   _githubUsername "$1"
+   _downloadKeys 
+   _writeKeys
+ } 
+
+ _runscript "$1"
+ exit 0
